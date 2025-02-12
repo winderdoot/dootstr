0835f4bdc3580678262c0db2790b5ae0bed31b07
winderdoot <01189157@pw.edu.pl>
2025-02-12 00:00:09 +0000

    Demo

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main()
 {
     dootstr_t *s1 = doot_new(0);
+    
     doot_insert_c(s1, "New string", 0);
     puts(s1->pstr);
     doot_insert_c(s1, "Newer string", 0);
@@ -15,7 +16,16 @@ int main()
     doot_insert_c(s1, "UwU", 20);
     puts(s1->pstr);
 
-
     doot_free(&s1);
+
+    dootstr_t s2 = {};
+    doot_assign_c(&s2, "Stack allocated string");
+    doot_append_c(&s2, " Just added more stuuuff!\n");
+    puts(s2.pstr);
+    doot_insert_c(&s2, "funky damn", 13);
+    puts(s2.pstr);
+
+    doot_destroy(&s2);
+    
     return EXIT_SUCCESS;
 }
